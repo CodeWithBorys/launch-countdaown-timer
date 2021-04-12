225b13d07292e09dd0ec72a7e95e71c223d83c03
Borys <bogori92@gmail.com>
2021-04-12 19:46:17 +0200

    seconds flip

--- scripts.js
+++ scripts.js
@@ -1,15 +1,25 @@
 'use strict';
 
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+const days = document.querySelector('.days .card-face-front');
+const hours = document.querySelector('.hours .card-face-front');
+const minutes = document.querySelector('.minutes .card-face-front');
+const seconds = document.querySelector('.seconds');
+const card = document.querySelector('.card');
 
-const timeLeft = {
+// .card-face-back
+
+const timeLeftPrev = {
   days: 0,
   hours: 0,
   minutes: 0,
   seconds: 0,
+};
+
+const timeLeftCur = {
+  days: 1,
+  hours: 1,
+  minutes: 1,
+  seconds: 1,
 };
 
 const dueDate = 1625097599999;
@@ -18,11 +28,20 @@ updateTime();
 
 function updateTime() {
   const timeNow = Date.now();
-  timeLeft.seconds = formatNum(((dueDate - timeNow) / 1000) % 60);
-  timeLeft.minutes = formatNum(((dueDate - timeNow) / (1000 * 60)) % 60);
-  timeLeft.hours = formatNum(((dueDate - timeNow) / (1000 * 60 * 60)) % 60);
-  timeLeft.days = formatNum((dueDate - timeNow) / (1000 * 60 * 60 * 24));
-  displayTime(timeLeft);
+  const secondsCountCur = formatNum(((dueDate - timeNow) / 1000) % 60);
+  const secondsCountPrev = formatNum(((dueDate - timeNow - 1000) / 1000) % 60);
+  if (timeLeftCur.seconds !== secondsCountCur) {
+    timeLeftPrev.seconds = secondsCountPrev;
+    timeLeftCur.seconds = secondsCountCur;
+    seconds.querySelector('.card').classList.add('flipped');
+  } else {
+    seconds.querySelector('.card').classList.remove('flipped');
+  }
+  timeLeftCur.minutes = formatNum(((dueDate - timeNow) / (1000 * 60)) % 60);
+  timeLeftCur.hours = formatNum(((dueDate - timeNow) / (1000 * 60 * 60)) % 60);
+  timeLeftCur.days = formatNum((dueDate - timeNow) / (1000 * 60 * 60 * 24));
+  displayTimeCur(timeLeftCur);
+  displayTimePrev(timeLeftPrev);
   setTimeout(updateTime, 1000);
 }
 
@@ -32,9 +51,20 @@ function formatNum(num) {
   return num;
 }
 
-function displayTime(obj) {
+function displayTimeCur(obj) {
   days.textContent = obj.days;
   hours.textContent = obj.hours;
   minutes.textContent = obj.minutes;
-  seconds.textContent = obj.seconds;
+  seconds.querySelector('.card-face-front').textContent = obj.seconds;
+  seconds
+    .querySelector('.digit')
+    .setAttribute('data-digit-before', obj.seconds);
+}
+
+function displayTimePrev(obj) {
+  // days.textContent = obj.days;
+  // hours.textContent = obj.hours;
+  // minutes.textContent = obj.minutes;
+  seconds.querySelector('.card-face-back').textContent = obj.seconds;
+  seconds.querySelector('.digit').setAttribute('data-digit-after', obj.seconds);
 }
